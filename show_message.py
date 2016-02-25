cfe4df516cdd6fa322fa26146519a55dcadfa868
Kelly Taylor <kajackdfw@yahoo.com>
2016-02-24 19:41:35 -0600

    Update show_message.py

--- show_message.py
+++ show_message.py
@@ -1,4 +1,6 @@
 from sense_hat import SenseHat
 
 sense = SenseHat()
-sense.show_message("Hello my name is Tim Peake")
+
+while True:
+    sense.show_message("Astro Pi is awesome!!", scroll_speed=0.05, text_colour=[255,255,0], back_colour=[0,0,255])
